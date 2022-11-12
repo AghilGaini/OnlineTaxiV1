11a3df5cea8fa75ba987765738e2d555a18ad8c8
asma <asma.gaini@yahoo.com>
2022-11-13 00:52:16 +0330

    Add MethodExtension project

---
+++ OnlineTaxi/CoreService/MethodExtensiion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreService
+{
+    public static class MethodExtensiion
+    {
+        public static long ToLong(this object o)
+        {
+            try
+            {
+                return (long)o;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        public static long? ToNullableLong(this object o)
+        {
+            try
+            {
+                return (long)o;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
